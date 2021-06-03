4d638274fa3dc9d11a951394e0a9439f1518b7c4
GitHubのユーザ名 <ranranluutennis@gmail.com>
2021-06-03 06:29:50 +0000

    [finish]3-デバッグ

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,13 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
+    p "標準出力にのみ反映"
+    logger.debug("標準出力とログファイルに記録される")
   end
 
   def show
     @blog = Blog.find(params[:id])
-    
+
   end
 
   def new
@@ -17,28 +19,29 @@ class BlogsController < ApplicationController
     blog.save
     redirect_to blog_path(blog.id)
   end
-  
-  
+
+
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
-  
+
   def destroy
     blog = Blog.find(params[:id])
     blog.destroy
     redirect_to blogs_path
   end
-  
-  
+
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
+
 
 end
